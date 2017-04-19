4f41a9c186b353e00307f8dbc699145cd3a6a986
Jared Cunha <jaredcunha@gmail.com>
2017-04-19 15:51:43 -0400

    add the form controls

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -7,4 +7,8 @@
  -{% include_relative vendor/uswds.min.js %}
 
  // Add your JS sripts here
+ -{% include_relative components/form-validation.js %}
+ -{% include_relative components/session-storage.js %}
+
+ // Add your JS sripts here
   -{% include_relative foo.js %}
---
+++ assets/javascripts/components/session-storage.js
@@ -0,0 +1,116 @@
+/*
+
+  Form functions for storing and retrieving data
+  for prototyping forms in static sites
+*/
+
+$(document).ready(function() {
+
+  $('form').on( "submit", function( event ) {
+    if ( this.checkValidity && !this.checkValidity() ) {
+      $( this ).find( ":invalid" ).first().focus().addClass('input-error');
+      event.preventDefault();
+    }
+  });
+
+  // Can get previously stored data to run events
+  getData = function() {
+    for (i = 0; i < sessionStorage.length; i++) {
+      if (sessionStorage.getItem(sessionStorage.key(i)) != "undefined") {
+        window[sessionStorage.key(i)] = sessionStorage.getItem(sessionStorage.key(i));
+      }
+      else {
+        window[sessionStorage.key(i)] = "";
+      }
+    }
+  }
+  // Initially run the function
+  getData();
+
+  //Save Data
+  storeData = function() {
+    $('input[type="tel"], input[type="number"], input[type="text"], input[type="email"], input[type="password"], select').each(function(){
+        var value = $(this).val();
+        var key = $(this).attr('id');
+        sessionStorage.setItem(key, value);
+    });
+
+    $("input[type='radio']:checked").each(function(){
+        var value = $(this).val();
+        var key = $(this).attr('name');
+        sessionStorage.setItem(key, value);
+    });
+    $("input[type='checkbox']").each(function(){
+        var value = "no";
+        var key = $(this).attr('id');
+        sessionStorage.setItem(key, value);
+    });
+    $("input[type='checkbox']:checked").each(function(){
+        var value = "yes";
+        var key = $(this).attr('id');
+        var set = $(this).attr('name');
+        sessionStorage.setItem(key, value);
+        var selections = $("input[type='checkbox']:checked").map(function(){
+          return $(this).attr('value');
+        }).get();
+        sessionStorage.setItem(set, selections);
+    });
+  }
+
+  // Simple function to go to the next page
+  nextPage = function(page) {
+    window.location.href = page;
+  }
+
+  printData = function() {
+    getData();
+
+    var $placeholder = $('[data-print]');
+
+    $placeholder.each(function() {
+      placeholder_key = $(this).attr('data-print');
+      var printed_text = window[placeholder_key];
+
+      var element_type = $(this).prop('nodeName').toLowerCase();
+
+      if ($(element_type).is('input')) {
+        if ($(this).attr("type") == "radio") {
+          if ($(this).attr("value") == printed_text) {
+            $(this).prop('checked', true);
+          }
+        }
+        if ($(this).attr("type") == "checkbox") {
+          if ( printed_text == "yes") {
+            $(this).prop('checked', true);
+          }
+        }
+        else {
+          $(this).attr('data-print', placeholder_key).val(printed_text);
+        }
+      }
+      else if ($(element_type).is('select')) {
+        $(this).find('option[value='+ printed_text +']').prop('selected', true);
+      }
+      else {
+        $(this).attr('data-print', placeholder_key).text(printed_text);
+      }
+    });
+
+  }
+  printData();
+
+  updateStoredData = function() {
+    storeData();
+    getData();
+  }
+
+  // For now, if browser does not support HTML5 validation, allow form data to be submitted without validation.
+  // In practice, this should be replaced with a shim for IE9.
+  checkValidityIfSupported = function(elem) {
+    if (typeof elem.checkValidity == 'function') {
+        return elem.checkValidity();
+    } else {
+        return true;
+    }
+  }
+});
