5f5ce093d06c1216b3e2177a1adaf800b021aa72
Jared Cunha <jaredcunha@gmail.com>
2017-05-08 12:45:54 -0400

    select boxes

--- assets/javascripts/components/followup-fields.js
+++ assets/javascripts/components/followup-fields.js
@@ -6,7 +6,8 @@
 $(document).ready(function() {
   var hidden_class = "usa-extend--hidden";
       $radio = $("input:radio");
-      $checkbox = $('input:checkbox[data-followup]');
+      $checkbox = $('input:checkbox[data-followup]'),
+      $selectbox = $('select');
 
   var showFollowup = function(e, required) {
     console.log(required);
@@ -49,6 +50,25 @@ $(document).ready(function() {
     }
     else {
       hideFollowup($('#' + target), required);
+    }
+  });
+
+  // Handle Selct Boxes
+  var previous;
+  $selectbox.on('click focus', function(){
+    previous = $(this).find(':selected').attr('data-followup'),
+    previous_required = $(this).find(':selected').attr('data-followup-required');
+  }).change(function(){
+    console.log(previous)
+    var selected_val = $(this).find('option[data-followup]:selected').val();
+    var target = $(this).find(':selected').attr('data-followup'),
+        required = $(this).find(':selected').attr('data-followup-required');
+
+    if ($(this).find(':selected').val() == selected_val) {
+      showFollowup($('#' + target), required);
+    }
+    if (selected_val != previous) {
+      hideFollowup($('#' + previous), previous_required);
     }
   });
 
