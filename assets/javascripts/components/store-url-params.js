021e220253bc5af597b946696c7b4000a3cb0fe0
Jared Cunha <jaredcunha@gmail.com>
2017-09-25 13:52:07 -0400

    store URL params

---
+++ assets/javascripts/components/store-url-params.js
@@ -0,0 +1,19 @@
+/*
+
+  Store URL Params:
+  This function will store URL params into sessionStorage for use with prototypes.
+*/
+
+$(document).ready(function() {
+function storeUrlParams() {
+  var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+  for(var i = 0; i < hashes.length; i++)
+  {
+    hash = hashes[i].split('=');
+    sessionStorage.setItem(hash[0], decodeURI(hash[1]));
+  }
+}
+
+storeUrlParams();
+
+});
